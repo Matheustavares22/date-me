8ca8b19658f51fcd00a55f31ae319ef2714e8695
Matheustavares22 <matheus.tav.nun@gmail.com>
2023-06-06 21:51:45 -0300

    cleaning code

--- date-me.py
+++ date-me.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import random
 
+
 #CONSTANTS
 BACKGROUND_COLOR = '#856ff8'
 DEFAULT_FONT_FAMILY = 'Calibri'
@@ -9,38 +10,43 @@ SCREEN_SIZE_WIDTH_HEIGHT = '450x300'
 BUTTON_FONT_SIZE = '12'
 MSG_FONT_SIZE = "16"
 
+
 class DateMe:
     def __init__(self, master=None):
         self.widget1 = Frame(master)
         self.widget1.configure(background=BACKGROUND_COLOR)
         self.widget1.pack()
+        
         self.msg = Label(self.widget1, text="Date me?")
         self.msg["font"] = (DEFAULT_FONT_FAMILY, MSG_FONT_SIZE, "bold")
-        self.msg['background']=BACKGROUND_COLOR
-        self.msg.configure(height=6)
+        self.msg.configure(background = BACKGROUND_COLOR, foreground='WHITE', height=6)
         self.msg.pack ()
-        self.yes = Button(self.widget1)
-        self.yes["text"] = "yes"
+        
+        self.yes = Button(self.widget1, text="yes")
         self.yes["font"] = (DEFAULT_FONT_FAMILY,BUTTON_FONT_SIZE)
-        self.yes["width"] = 5
-        self.yes["command"] = self.happyness
+        self.yes.configure(background=BACKGROUND_COLOR,command=self.happyness, width = 5)
         self.yes.pack (side=LEFT)
-        self.no = Button(self.widget1)
-        self.no["text"] = "no"
+        
+        self.no = Button(self.widget1, text= "no")
         self.no["font"] = (DEFAULT_FONT_FAMILY, BUTTON_FONT_SIZE)
-        self.no["width"] = 5
-        self.no["command"] = self.runAwayButton
+        self.no.configure(background= BACKGROUND_COLOR, command = self.runAwayButton, width= 5)
         self.no.pack (side=RIGHT)
     def runAwayButton():
-        a
+        a=1
+
         
     def happyness():
-        a
+        a=1
 
-root = Tk(className='Date-me')
 
-root.geometry(SCREEN_SIZE_WIDTH_HEIGHT)
-root['background']=BACKGROUND_COLOR
-root.eval('tk::PlaceWindow . center')
-DateMe(root)
-root.mainloop()
+def main():
+    root = Tk(className='Date-me')
+    root.geometry(SCREEN_SIZE_WIDTH_HEIGHT)
+    root.configure(background= BACKGROUND_COLOR)
+    root.eval('tk::PlaceWindow . center')
+    DateMe(root)
+    root.mainloop()
+    
+    
+if __name__ == '__main__':
+    main()
